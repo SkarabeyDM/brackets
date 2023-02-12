e272e26e2b2aea0b0a5bcf399d533f433b0ed6ea
Skarabey DM <mechanoid0415@gmail.com>
2023-02-12 22:21:49 +0300

    refactor: simplify code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-/* const config1 = [['(', ')']];
-const config2 = [['(', ')'], ['[', ']']];
-const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
-const config4 = [['|', '|']];
-const config5 = [['(', ')'], ['|', '|']];
-const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
-const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']]; */
-
 /**
  * Is bracket nesting correct.
  * @param {string} str 
@@ -15,26 +7,14 @@ const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']]; */
 function check(str, bracketsConfig)
 {
   const stack = []
-
-  // Read config
-  const [opens, closes] = bracketsConfig.reduce(([openList, closeList], [open, close]) => ([{ ...openList, [open]: close }, { ...closeList, [close]: open }]), [{}, {}])
-
-  // Check nesting
-  const isOpen = (bracket) => bracket in opens
-  const isClose = (bracket) => bracket in closes
-  const isRequired = (bracket) => opens[stack[stack.length - 1]] === bracket        // Is current bracket closes previous
+  const config = Object.fromEntries(bracketsConfig)
 
   for (const bracket of str)
-    if (isOpen(bracket))
-      if (isClose(bracket)) isRequired(bracket) ? stack.pop() : stack.push(bracket) // If bracket is from "twins" pair. Example: ["|", "|"]
-      else stack.push(bracket)                                                      // If bracket is just open 
-    else if (isClose(bracket) && isRequired(bracket)) stack.pop()                   // If bracket is closing and correct
+    if (bracket === config[stack[stack.length - 1]]) stack.pop()
+    else if (bracket in config) stack.push(bracket)
     else return false
 
   return !stack.length;
 }
-
-/* console.log(check("([{}])",config3)) */
-/* console.log(check('|()|', config5)) */
 
 module.exports = check
